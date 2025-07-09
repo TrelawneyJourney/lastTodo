8e3fc2975b80d7f4ebaef775e3888adc59482d41
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-07-09 18:30:02 -0300

    correccion ruta img

--- client/src/components/Start.jsx
+++ client/src/components/Start.jsx
@@ -1,7 +1,10 @@
+import inicioSvg from "../assets/inicio.svg";
+
 export default function Start() {
   return (
     <div className="w-full">
-      <img src="./src/assets/inicio.svg" alt="" className="opacity-70" />
+      <img src={inicioSvg} alt="" className="opacity-70" />
+      {/* <img src="./src/assets/inicio.svg" alt="" className="opacity-70" /> */}
     </div>
   );
 }
